cb00cc3b22c3787de680527909436d18c363bb66
Zach Williams <hey@zachwill.com>
2011-09-24 18:00:32 -0700

    Add header to cache page for 10 minutes

--- app/views.py
+++ app/views.py
@@ -40,8 +40,12 @@ def qunit():
 
 @views.after_request
 def add_header(response):
-    """Add header to force latest IE rendering engine and Chrome Frame."""
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
     response.headers['X-UA-Compatible'] = 'IE=Edge,chrome=1'
+    response.headers['Cache-Control'] = 'public, max-age=600'
     return response
 
 
